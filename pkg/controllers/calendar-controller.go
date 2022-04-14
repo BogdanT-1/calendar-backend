1d1443da08b468095e25216daf4b6c44eb2263f7
bogdan.torjea <bogdan.torjea@qubiz.ro>
2022-04-14 23:15:05 +0300

    first commit

---
+++ pkg/controllers/calendar-controller.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/BogdanT-1/calendar-backend/pkg/models"
+	"github.com/BogdanT-1/calendar-backend/pkg/utils"
+	"github.com/gorilla/mux"
+)
+
+var NewTask models.Task
+
+func GetTasks(w http.ResponseWriter, r *http.Request){
+	tasks:=models.GetAllTasks()
+	res, _ :=json.Marshal(tasks)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetTaskById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	taskId := vars["taskId"]
+	ID, err:= strconv.ParseInt(taskId,0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	taskDetails, _:= models.GetTaskById(ID)
+	res, _ := json.Marshal(taskDetails)
+	w.Header().Set("Content-Type","pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request){
+	CreateTask := &models.Task{}
+	utils.ParseBody(r, CreateTask)
+	b:= CreateTask.CreateTask()
+	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	taskId := vars["taskId"]
+	ID, err := strconv.ParseInt(taskId, 0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	task := models.DeleteTask(ID)
+	res, _ := json.Marshal(task)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateTask(w http.ResponseWriter, r *http.Request){
+	var updateTask = &models.Task{}
+	utils.ParseBody(r, updateTask)
+	vars := mux.Vars(r)
+	taskId := vars["taskId"]
+	ID, err := strconv.ParseInt(taskId, 0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	taskDetails, db:=models.GetTaskById(ID)
+	taskDetails = updateTask;
+	db.Save(&taskDetails)
+	res, _ := json.Marshal(taskDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
---
+++ pkg/routes/calendar-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/BogdanT-1/calendar-backend/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterCalendarRoutes = func(router *mux.Router){
+	router.HandleFunc("/task/", controllers.CreateTask).Methods("POST")
+	router.HandleFunc("/tasks/", controllers.GetTasks).Methods("GET")
+	router.HandleFunc("/task/{taskId}", controllers.GetTaskById).Methods("GET")
+	router.HandleFunc("/task/{taskId}", controllers.UpdateTask).Methods("PUT")
+	router.HandleFunc("/task/{taskId}", controllers.DeleteTask).Methods("DELETE")
+}
